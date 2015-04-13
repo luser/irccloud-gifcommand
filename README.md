0820ee61346264ea13c72df57281ffcf8a4da50e
Ted Mielczarek <ted@mielczarek.org>
2015-04-13 08:52:54 -0400

    Add userscript metadata, minor fixup to work as user script

---
+++ README.md
@@ -0,0 +1,2 @@
+This user script implements a /gif command for [irccloud](https://www.irccloud.com/). You can type `/gif word` and the script will search [Giphy](http://giphy.com/) for a matching gif and send the URL to the current channel.
+
--- irccloud_gif.user.js
+++ irccloud_gif.user.js
@@ -1,3 +1,17 @@
+/* Any copyright is dedicated to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+/*global Promise, unsafeWindow */
+// ==UserScript==
+// @name irccloud gif command
+// @namespace https://github.com/luser/irccloud-gifcommand
+// @description Add a /gif command to irccloud
+// @downloadURL https://raw.githubusercontent.com/luser/irccloud-gifcommand/master/irccloud_gif.user.js
+// @version 1
+// @match https://www.irccloud.com/*
+// @match https://irccloud.mozilla.com/*
+// @noframes
+// ==/UserScript==
+
 function getGIF(words) {
   return new Promise(function(resolve, reject) {
     var req = new XMLHttpRequest();
@@ -22,8 +36,8 @@ var _COMMANDS = {
   gif: getGIF
 };
 
-var _oldsay = SESSIONVIEW.mainArea.current.input.__proto__.say;
-SESSIONVIEW.mainArea.current.input.__proto__.say = function(m) {
+var _oldsay = unsafeWindow.SESSIONVIEW.mainArea.current.input.__proto__.say;
+unsafeWindow.SESSIONVIEW.mainArea.current.input.__proto__.say = function(m) {
   var cmd = m.split(' ')[0].substr(1);
   if (m.startsWith('/') && _COMMANDS.hasOwnProperty(cmd)) {
     this.clear();
